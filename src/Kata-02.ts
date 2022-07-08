f2778e90f2a222112443dd57ff794b24503c7583
Roger David Alba Ortega <rogerdavidalbaortega@gmail.com>
2022-07-07 22:09:20 -0500

    Solve kata-02 Serie Fibonacci

---
+++ src/Kata-02.ts
@@ -0,0 +1,31 @@
+/*
+ * Reto #2
+ * LA SUCESIÓN DE FIBONACCI
+ * Fecha publicación enunciado: 10/01/22
+ * Fecha publicación resolución: 17/01/22
+ * Dificultad: DIFÍCIL
+ *
+ * Enunciado: Escribe un programa que imprima los 50 primeros números de la sucesión de Fibonacci empezando en 0.
+ * La serie Fibonacci se compone por una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores.
+ * 0, 1, 1, 2, 3, 5, 8, 13...
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+const numbers:number = 50;
+let a:number = 0;
+let b:number = 1;
+let fibonacci:number=0;
+console.log(a);
+console.log(b);
+for(let i=2;i<=numbers;i++){
+  fibonacci = a+b;
+  a=b;
+  b=fibonacci;
+  console.log(fibonacci);
+
+}
